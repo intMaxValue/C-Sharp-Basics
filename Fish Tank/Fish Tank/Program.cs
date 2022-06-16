9a86420d154356cb3acbd73558b84b4b13facbea
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-06-16 12:46:30 +0300

    Add files via upload

---
+++ Fish Tank/Fish Tank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double volume = (lenght * width * height) * 0.001;
+            double accessories = percent / 100;
+            double volumeWithAccessories = volume * (1 - accessories);
+
+            Console.WriteLine(volumeWithAccessories);
+        }
+    }
+}
