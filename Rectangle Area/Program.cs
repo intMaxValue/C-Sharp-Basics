9a0b52fbe6feb947ff11d9c351f8b3a897fe93e3
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-06-20 13:13:00 +0300

    Add files via upload

---
+++ Rectangle Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double cm = 2.54;
+            double area = inches * cm;
+            Console.WriteLine(area);
+        }
+    }
+}
