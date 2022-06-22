dd8344cb029cd048b1282d80e518edae4ff247b3
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-06-22 13:53:50 +0300

    Add files via upload

---
+++ Pet Shop/Pet Shop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dogfood = 2.50;
+            double catfood = 4;
+           double cf Console.ReadLine(dogfood + )
+
+        }
+    }
+}
