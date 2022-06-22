2604796c3afc2fbe651f40437e5c82994f8bc1d7
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-06-22 13:54:54 +0300

    Add files via upload

---
+++ YardGreening/YardGreening/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TESTLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pricePerSquareMeter = 7.61;
+            double discountPerSquareMeter = 0.18;
+            double squareMeters = double.Parse(Console.ReadLine());
+            double totalPrice = squareMeters * pricePerSquareMeter;
+            double totalDiscount = discountPerSquareMeter * totalPrice;
+            double totalPriceWithDiscount = totalPrice - totalDiscount;
+
+            Console.WriteLine("The final price is: " + totalPriceWithDiscount + " lv.");
+            Console.WriteLine("The discount is: " + totalDiscount + " lv.");
+
+
+        }
+    }
+}
