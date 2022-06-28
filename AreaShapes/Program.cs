61d9e75aa3fa6e91b133ae70255c200169382119
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-06-28 14:44:55 +0300

    Add files via upload

---
+++ AreaShapes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AreaShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double area = 0;
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = side * side;
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB;
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = radius * radius * Math.PI; 
+            }
+            else if (figure == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                area = (sideA * height) / 2;
+            }
+
+            Console.WriteLine("{0:F3}", area);
+        }
+    }
+}
---
+++ OneToNine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OneToNine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 9)
+            {
+                Console.WriteLine("number too big");
+            }
+        }
+    }
+}
