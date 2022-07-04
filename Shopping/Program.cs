b6d63e89a9436455cc46ab2919af89578d3d210d
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-07-04 12:16:02 +0300

    Add files via upload

---
+++ Shopping/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int gpuCount = int.Parse(Console.ReadLine());
+            int cpuCount = int.Parse(Console.ReadLine());
+            int ramCount = int.Parse(Console.ReadLine());
+
+            double gpuPrice = 250;
+            double cpuPrice = 0.35 * (gpuCount * gpuPrice);
+            double ramPrice = 0.1 * (gpuCount * gpuPrice);
+
+            double totalPrice = gpuCount * gpuPrice + cpuCount * cpuPrice + ramCount * ramPrice;
+
+            if (gpuCount > cpuCount)
+            {
+                totalPrice = totalPrice * 0.85;
+            }
+
+            if (budget >= totalPrice)
+            {
+                Console.WriteLine($"You have {budget - totalPrice:F2} leva left!");
+            }
+
+            else if (budget < totalPrice)
+            {
+                Console.WriteLine($"Not enough money! You need {totalPrice - budget:F2} leva more!");
+            }
+        }
+    }
+}
---
+++ SwimingWorldRecord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwimingWorldRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeInSecondsFor1Meter = double.Parse(Console.ReadLine());
+
+            double totalTimeSwiming = distanceInMeters * timeInSecondsFor1Meter;
+            double resistance = Math.Floor(distanceInMeters / 15) * 12.5;
+            double totalTimeWithWaterResistance = totalTimeSwiming + resistance;
+
+            if (totalTimeWithWaterResistance >= recordInSeconds)
+            {
+                Console.WriteLine($"No, he failed! He was {totalTimeWithWaterResistance - recordInSeconds:F2} seconds slower.");
+            }
+
+            else if (totalTimeWithWaterResistance < recordInSeconds)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTimeWithWaterResistance:F2} seconds.");
+            }
+        }
+    }
+}
