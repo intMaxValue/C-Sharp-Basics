4cefd53415f42f61cc09fe8a02416d63c06f8cf0
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-07-09 20:04:49 +0300

    Add files via upload

---
+++ WorkingTime/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace WorkingTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string dayOfTheWeek = Console.ReadLine();
+
+            if (hour >= 10 && hour <= 18)
+            {
+                if (dayOfTheWeek == "Monday")
+                {
+                    Console.WriteLine("open");
+                }
+                else if (dayOfTheWeek == "Tuesday")
+                {
+                    Console.WriteLine("open");
+                }
+                else if (dayOfTheWeek == "Wednesday")
+                {
+                    Console.WriteLine("open");
+                }
+                else if (dayOfTheWeek == "Thursday")
+                {
+                    Console.WriteLine("open");
+                }
+                else if (dayOfTheWeek == "Friday")
+                {
+                    Console.WriteLine("open");
+                }
+                else if (dayOfTheWeek == "Saturday")
+                {
+                    Console.WriteLine("open");
+                }
+                else
+                {
+                    Console.WriteLine("closed");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
