6855a5b4bd518c776bbc6a2304954d298fc7ac49
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-08-17 15:21:40 +0300

    Add files via upload

---
+++ Cinema/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeProjection = Console.ReadLine();
+            int rowCount = int.Parse(Console.ReadLine());
+            int columnCount = int.Parse(Console.ReadLine());
+
+            int totalSeats = rowCount * columnCount;
+
+            if (typeProjection == "Premiere")
+            {
+                double income = totalSeats * 12.00;
+                Console.WriteLine($"{income:F2} leva");
+            }
+
+            else if (typeProjection == "Normal")
+            {
+                double income = totalSeats * 7.50;
+                Console.WriteLine($"{income:F2} leva");
+            }
+
+            else if (typeProjection == "Discount")
+            {
+                double income = totalSeats * 5.00;
+                Console.WriteLine($"{income:F2} leva");
+            }
+        }
+    }
+}
---
+++ FishingBoat/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace FishingBoat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermansCount = int.Parse(Console.ReadLine());
+
+
+            if (season == "Spring")
+            {
+                double rent = 3000;
+                if (fishermansCount <= 6)
+                {
+                    rent *= 0.9;
+                }
+                else if (fishermansCount > 7 && fishermansCount <= 11)
+                {
+                    rent *= 0.85;
+                }
+                else if (fishermansCount > 12)
+                {
+                    rent *= 0.75;
+                }
+
+                if (fishermansCount % 2 == 0 && budget >= rent)
+                {
+                    double discount = budget - (rent * 0.95);
+                    Console.WriteLine($"Yes! You have {discount:F2} leva left.");
+                }
+                else if (fishermansCount % 2 == 0 && budget < rent)
+                {
+                    double moneyNeeded = (rent * 0.95) - budget;
+                    Console.WriteLine($"Not enough money! You need {moneyNeeded:F2} leva.");
+                }
+                else if (fishermansCount % 2 != 0 && budget >= rent)
+                {
+                    double moneyLeft = budget - rent;
+                    Console.WriteLine($"Yes! You have {moneyLeft:F2} leva left.");
+                }
+                else if (fishermansCount % 2 != 0 && budget < rent)
+                {
+                    double moneyNeeded = (rent - budget);
+                    Console.WriteLine($"Not enough money! You need {moneyNeeded:F2} leva.");
+                }
+
+                
+
+            }
+            else if (season == "Summer" || season == "Autumn")
+            {
+                double rent = 4200;
+                if (fishermansCount <= 6)
+                {
+                    rent *= 0.9;
+                }
+                else if (fishermansCount > 7 && fishermansCount <= 11)
+                {
+                    rent *= 0.85;
+                }
+                else if (fishermansCount > 12)
+                {
+                    rent *= 0.75;
+                }
+
+                if (fishermansCount % 2 == 0 && budget >= rent && season == "Summer")
+                {
+                    double discount = budget - (rent * 0.95);
+                    Console.WriteLine($"Yes! You have {discount:F2} leva left.");
+                }
+                else if (fishermansCount % 2 == 0 && budget < rent && season == "Summer")
+                {
+                    double moneyNeeded = (rent - budget);
+                    Console.WriteLine($"Not enough money! You need {moneyNeeded:F2} leva.");
+                }
+                else if (fishermansCount % 2 != 0 && budget >= rent)
+                {
+                    double moneyLeft = budget - rent;
+                    Console.WriteLine($"Yes! You have {moneyLeft:F2} leva left.");
+                }
+                else if (fishermansCount % 2 != 0 && budget < rent)
+                {
+                    double moneyNeeded = (rent - budget);
+                    Console.WriteLine($"Not enough money! You need {moneyNeeded:F2} leva.");
+                }
+                else if (fishermansCount % 2 == 0 && budget < rent)
+                {
+                    double moneyNeeded = (rent - budget);
+                    Console.WriteLine($"Not enough money! You need {moneyNeeded:F2} leva.");
+                }
+
+
+            }
+            else if (season == "Winter")
+            {
+                double rent = 2600;
+                if (fishermansCount <= 6)
+                {
+                    rent *= 0.9;
+                }
+                else if (fishermansCount > 7 && fishermansCount <= 11)
+                {
+                    rent *= 0.85;
+                }
+                else if (fishermansCount > 12)
+                {
+                    rent *= 0.75;
+                }
+
+                if (fishermansCount % 2 == 0 && budget >= rent)
+                {
+                    double discount = budget - (rent * 0.95);
+                    Console.WriteLine($"Yes! You have {discount:F2} leva left.");
+                }
+                else if (fishermansCount % 2 == 0 && budget < rent)
+                {
+                    double moneyNeeded = (rent - budget);
+                    Console.WriteLine($"Not enough money! You need {moneyNeeded:F2} leva.");
+                }
+                else if (fishermansCount % 2 != 0 && budget >= rent)
+                {
+                    double moneyLeft = budget - rent;
+                    Console.WriteLine($"Yes! You have {moneyLeft:F2} leva left.");
+                }
+                else if (fishermansCount % 2 != 0 && budget < rent)
+                {
+                    double moneyNeeded = (rent - budget);
+                    Console.WriteLine($"Not enough money! You need {moneyNeeded:F2} leva.");
+                }
+            }
+
+
+
+        }
+    }
+}
