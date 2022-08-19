b1dd5e9199a7b40861ac840fc5130692663e1496
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-08-19 15:13:26 +0300

    Add files via upload

---
+++ OnTimeForExam/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace OnTimeForExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hourExamStarts = int.Parse(Console.ReadLine());
+            int minuteExamStarts = int.Parse(Console.ReadLine());
+            int hourOfArrival = int.Parse(Console.ReadLine());
+            int minuteOfArrival = int.Parse(Console.ReadLine());
+
+            int examStartInMinutes = (60 * hourExamStarts) + minuteExamStarts;
+            int arrivalInMinutes = (60 * hourOfArrival) + minuteOfArrival;
+
+            int lateWithHours = (arrivalInMinutes - examStartInMinutes) / 60;
+            int lateWithMinutes = (arrivalInMinutes - examStartInMinutes) % 60;
+
+            int earlyWithHours = (examStartInMinutes - arrivalInMinutes) / 60;
+            int earlyWithMinutes = (examStartInMinutes - arrivalInMinutes) % 60;
+
+            if (arrivalInMinutes > examStartInMinutes)
+            {
+                Console.WriteLine("Late");
+            }
+            else if (arrivalInMinutes == examStartInMinutes) 
+            {
+                Console.WriteLine("On time");
+            }
+            if (earlyWithMinutes <= 30 && earlyWithMinutes > 0 && earlyWithHours == 0)
+            {
+                Console.WriteLine("On time");
+            }
+            else if (earlyWithMinutes > 30 || earlyWithHours > 0)
+            {
+                Console.WriteLine("Early");
+            }
+
+
+            if (arrivalInMinutes != examStartInMinutes)
+            {
+
+                if (arrivalInMinutes > examStartInMinutes)
+                {
+                    if (lateWithHours == 0)
+                    {
+                        Console.WriteLine($"{lateWithMinutes} minutes after the start");
+                    }
+                    else if (lateWithHours > 0 && lateWithMinutes < 10)
+                    {
+                        Console.WriteLine($"{lateWithHours}:0{lateWithMinutes} hours after the start");
+                    }
+                    else if (lateWithHours > 0 && lateWithMinutes >= 10)
+                    {
+                        Console.WriteLine($"{lateWithHours}:{lateWithMinutes} hours after the start");
+                    }
+                }
+
+                if (arrivalInMinutes < examStartInMinutes)
+                {
+                    if (earlyWithHours == 0)
+                    {
+                        Console.WriteLine($"{earlyWithMinutes} minutes before the start");
+                    }
+                    else if (earlyWithHours > 0 && earlyWithMinutes < 10)
+                    {
+                        Console.WriteLine($"{earlyWithHours}:0{earlyWithMinutes} hours before the start");
+                    }
+                    else if (earlyWithHours > 0 && earlyWithMinutes >= 10)
+                    {
+                        Console.WriteLine($"{earlyWithHours}:{earlyWithMinutes} hours before the start");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace OperationsBetweenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+
+            if (@operator == "+")
+            {
+                double result = n1 + n2;
+
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} + {n2} = {result} - even");
+                }
+                else if (result % 2 != 0)
+                {
+                    Console.WriteLine($"{n1} + {n2} = {result} - odd");
+                }
+
+            }
+
+            else if (@operator == "-")
+            {
+                double result = n1 - n2;
+
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} - {n2} = {result} - even");
+                }
+                else if (result % 2 != 0)
+                {
+                    Console.WriteLine($"{n1} - {n2} = {result} - odd");
+                }
+
+            }
+
+            else if (@operator == "*")
+            {
+                double result = n1 * n2;
+
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} * {n2} = {result} - even");
+                }
+                else if (result % 2 != 0)
+                {
+                    Console.WriteLine($"{n1} * {n2} = {result} - odd");
+                }
+
+            }
+
+            else if (@operator == "/")
+            {
+                double result = n1 / n2;
+
+                if (n1 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {n2} by zero");
+                }
+                else if (n2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {n1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} / {n2} = {result:F2}");
+                }
+                
+
+            }
+
+            else if (@operator == "%")
+            {
+                double result = n1 % n2;
+
+                if (n1 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {n2} by zero");
+                }
+                else if (n2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {n1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} % {n2} = {result}");
+                }
+
+            }
+        }
+
+    }
+}
