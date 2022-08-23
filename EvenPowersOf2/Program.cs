7c49541dfa031659f49aa21848ba2a949a0414d0
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-08-23 16:27:53 +0300

    Add files via upload

---
+++ EvenPowersOf2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EvenPowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int result = 1;
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(result);
+                }
+
+                result *= 2;
+            }
+        }
+    }
+}
---
+++ HalfSumElement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxValue = int.MinValue;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                sum += input;
+                if (input > maxValue)
+                {
+                    maxValue = input;
+                }
+
+
+            }
+            int sumWithoutMaxValue = sum - maxValue;
+            int diff = Math.Abs(maxValue - sumWithoutMaxValue);
+
+            if (maxValue == sumWithoutMaxValue)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxValue}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
