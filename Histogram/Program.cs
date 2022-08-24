f4edb34616e00db7844c32babe07ef23f77fb8b2
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-08-24 13:55:03 +0300

    Add files via upload

---
+++ Histogram/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCount = int.Parse(Console.ReadLine());
+
+            double lessThan200 = 0;
+            double between200And399 = 0;
+            double between400And599 = 0;
+            double between600And799 = 0;
+            double moreThan800 = 0;
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (input < 200)
+                {
+                    lessThan200++;
+                }
+                else if (input >= 200 && input <= 399)
+                {
+                    between200And399++;
+                }
+                else if (input >= 400 && input <= 599)
+                {
+                    between400And599++;
+                }
+                else if (input >= 600 && input <= 799)
+                {
+                    between600And799++;
+                }
+                else if (input >= 800)
+                {
+                    moreThan800++;
+                }
+            }
+            double first = lessThan200 / numberCount * 100;
+            Console.WriteLine($"{first:F2}%");
+
+            double second = between200And399 / numberCount * 100;
+            Console.WriteLine($"{second:F2}%");
+
+            double third = between400And599 / numberCount * 100;
+            Console.WriteLine($"{third:F2}%");
+
+            double fourth = between600And799 / numberCount * 100;
+            Console.WriteLine($"{fourth:F2}%");
+
+            double fifth = moreThan800 / numberCount * 100;
+            Console.WriteLine($"{fifth:F2}%");
+        }
+    }
+}
---
+++ LeftAndRightSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                leftSum += number;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                rightSum += number;
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                int difference = Math.Abs(rightSum - leftSum);
+                Console.WriteLine($"No, diff = {difference}");
+            }
+        }
+    }
+}
