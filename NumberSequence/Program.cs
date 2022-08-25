83d4e13f4a56a06c7ee13dea55465464101c1e20
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-08-25 14:41:38 +0300

    Add files via upload

---
+++ NumberSequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCount = int.Parse(Console.ReadLine());
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber; 
+                }
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
