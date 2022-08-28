d6dc1bc34489bdde25c1a995bb6e592876b9fad1
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-08-28 19:47:26 +0300

    Add files via upload

---
+++ NumbersN1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumbersN1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = int.Parse(Console.ReadLine()); i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ OddEvenSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCount = int.Parse(Console.ReadLine());
+            int sumOdd = 0;
+            int sumEven = 0;
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int oddNum = int.Parse(Console.ReadLine());
+                    sumOdd += oddNum;
+                }
+                else
+                {
+                    int evenNum = int.Parse(Console.ReadLine());
+                    sumEven += evenNum;
+                }
+                
+            }
+
+            if (sumEven == sumOdd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = {0}", sumOdd);
+            }
+            else
+            {
+                int difference = Math.Abs(sumEven - sumOdd);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = {0}", Math.Abs(difference));
+            }
+        }
+    }
+}
