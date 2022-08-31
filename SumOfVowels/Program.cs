52ca4c729d89b3bb86b8bb522b218e244d3e67c0
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-08-31 14:44:34 +0300

    Add files via upload

---
+++ SumOfVowels/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfVowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int length = text.Length;
+            int sum = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                switch (text[i])
+                {
+                    case 'a': sum += 1; break;
+                    case 'e': sum += 2; break;
+                    case 'i': sum += 3; break;
+                    case 'o': sum += 4; break;
+                    case 'u': sum += 5; break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
