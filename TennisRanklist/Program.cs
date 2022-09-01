011aa72b1cb64f2365c1411018951420ca4461b2
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-09-01 17:38:19 +0300

    Add files via upload

---
+++ TennisRanklist/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TennisRanklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tournamentCount = int.Parse(Console.ReadLine());
+            double startPoints = int.Parse(Console.ReadLine());
+
+            string placement = "";
+            double totalPoints = startPoints;
+            double tournamentWon = 0;
+
+            for (int i = 0; i < tournamentCount; i++)
+            {
+                placement = Console.ReadLine();
+
+                if (placement == "W")
+                {
+                    totalPoints += 2000;
+                    tournamentWon++;
+                }
+                else if (placement == "F")
+                {
+                    totalPoints += 1200;
+                }
+                else if (placement == "SF")
+                {
+                    totalPoints += 720;
+                }
+
+            }
+
+            double averagePoints = (totalPoints - startPoints) / tournamentCount;
+            double percentOfTournamentsWon = (tournamentWon / tournamentCount) * 100;
+
+            Console.WriteLine($"Final points: {totalPoints}");
+            Console.WriteLine($"Average points: {averagePoints}");
+            Console.WriteLine($"{percentOfTournamentsWon:F2}%");
+        }
+    }
+}
