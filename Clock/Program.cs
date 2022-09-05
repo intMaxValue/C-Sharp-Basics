830d867a776e6576f1418bfc1dd0ac234c783dda
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-09-05 13:53:38 +0300

    Add files via upload

---
+++ Clock/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int h = 0; h < 24; h++)
+            {
+                for (int m = 0; m < 60; m++)
+                {
+                    if (h < 10 && m < 10)
+                    {
+                        Console.WriteLine($"0{h}:0{m}");
+                    }
+                    else if (h < 10 && m >= 10)
+                    {
+                        Console.WriteLine($"0{h}:{m}");
+                    }
+                    else if (h >= 10 && m < 10)
+                    {
+                        Console.WriteLine($"{h}:0{m}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{h}:{m}");
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
