6dcf80203ceaf0c56a8816a3cf5629bea4faaa87
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-09-17 12:43:45 +0300

    Add files via upload

---
+++ Travelling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                if (destination == "End")
+                {
+                    break;
+                }
+
+                double expenses = double.Parse(Console.ReadLine()),
+                savings = 0;
+
+                while (savings < expenses)
+                {
+                    double money = double.Parse(Console.ReadLine());
+                    savings += money;
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+
+            }
+        }
+    }
+}
