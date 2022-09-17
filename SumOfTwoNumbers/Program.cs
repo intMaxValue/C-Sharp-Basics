8efbc461e98d0a6e01c8208321efc96467d242df
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-09-17 12:06:32 +0300

    Add files via upload

---
+++ SumOfTwoNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int combinations = 0;
+            int sum = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = a; j <= b; j++)
+                {
+                    sum = i + j;
+                    combinations++;
+
+                    if (sum == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combinations} ({i} + {j} = {magicNumber})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{combinations} combinations - neither equals {magicNumber}");
+
+
+
+        }
+    }
+}
+
+
+
+
+
