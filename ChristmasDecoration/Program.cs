f09c3d00090739d953d6788471f7549277e70ab8
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2022-09-21 16:09:52 +0300

    Add files via upload

---
+++ ChristmasDecoration/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChristmasDecoration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string toyName = Console.ReadLine();
+
+            while (toyName != "Stop")
+            {
+
+                int toyPrice = 0;
+
+                for (int i = 0; i < toyName.Length; i++)
+                {
+                    toyPrice += toyName[i];
+                }
+
+                toyName = Console.ReadLine();
+            }
+        }
+    }
+}
